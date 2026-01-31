b50ca0cebd3336755a592dabfd1f828a7b4762bf
jaysankar1818 <33759857+jaysankar1818@users.noreply.github.com>
2026-01-30 21:18:14 -0600

    updateTest

--- playwrightautomationbatch2/src/main/java/test/Test.java
+++ playwrightautomationbatch2/src/main/java/test/Test.java
@@ -1,5 +1,29 @@
 package test;
 
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
 public class Test {
 
+public static void main(String[] args) {
+		
+		SoftAssert sa = new SoftAssert();
+
+	    sa.assertEquals("Login", "Login123");
+	    System.out.println("Execution1 is over");
+
+	    sa.assertEquals("Login", "Login123");
+	    System.out.println("Execution2 is over");
+	    
+	    String actualTitle = "Dashboard";
+	    
+	    Assert.assertEquals(actualTitle, "Dashboard");
+	    System.out.println("This will execute");
+
+	    System.out.println("all Execution is over");
+	    
+	    sa.assertAll(); // Mandatory
+	}
+	
+	
 }
